d7edffc71db8b200eab0f68b26fddbbabab1d0c4
Mark <poussard.mark@gmail.com>
2020-05-20 10:37:11 +0200

    Updates

---
+++ precache-manifest.7e12f1456cc0cbb74d9fb72072dee463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "55037f200ea34056f93d",
+    "url": "/portfolio/static/js/main.55037f20.chunk.js"
+  },
+  {
+    "revision": "40eed039df7dcd0183c5",
+    "url": "/portfolio/static/js/1.40eed039.chunk.js"
+  },
+  {
+    "revision": "55037f200ea34056f93d",
+    "url": "/portfolio/static/css/main.794250eb.chunk.css"
+  },
+  {
+    "revision": "9b20ae9e13e77cbf0e3beb8bb13544dd",
+    "url": "/portfolio/index.html"
+  }
+];
