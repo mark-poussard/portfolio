2bf599eddf5740d0f1c7d6161e714049c2edca33
Mark <poussard.mark@gmail.com>
2020-06-30 14:05:42 +0200

    Updates

--- precache-manifest.747dca10231e6814de9f050033795555.js
+++ precache-manifest.747dca10231e6814de9f050033795555.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "3baff69fa3ce663c85c9",
-    "url": "/portfolio/static/js/main.3baff69f.chunk.js"
+    "revision": "b484558960e55838eadd",
+    "url": "/portfolio/static/js/main.b4845589.chunk.js"
   },
   {
     "revision": "1122cb5ec2a78a9ce98f",
     "url": "/portfolio/static/js/1.1122cb5e.chunk.js"
   },
   {
-    "revision": "3baff69fa3ce663c85c9",
+    "revision": "b484558960e55838eadd",
     "url": "/portfolio/static/css/main.988fdf71.chunk.css"
   },
   {
-    "revision": "2730f34ae8f50140659551d46f13225d",
+    "revision": "da993203705f7ddf60c2a76da283e139",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.afdb5c523944c02ccbdee9c395e2c359.js"
+  "/portfolio/precache-manifest.747dca10231e6814de9f050033795555.js"
 );
 
 workbox.clientsClaim();
