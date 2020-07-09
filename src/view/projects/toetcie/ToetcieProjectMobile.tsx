dcbb058364dc7dba323691a5e108e857a0e50350
Kim Anh Nguyen <kimanhou@gmail.com>
2020-07-09 18:04:47 +0200

    Translation

--- src/view/projects/toetcie/ToetcieProjectMobile.tsx
+++ src/view/projects/toetcie/ToetcieProjectMobile.tsx
@@ -25,10 +25,10 @@ const ToetcieProjectMobile : React.FC = props => {
                 en : 
                     <>
                         <p>
-                            Une étude du marché et des statistiques d'utilisation nous dévoile que plus de 75% des utilisateurs naviguent sur le site web en utilisant des supports mobiles.
+                            The statistics showed us that 75% of users accessed the existing website via mobile.
                         </p>
                         <p>
-                            Face à cette réalité, il est absolument essentiel de proposer une solution réactive capable de s'adapter à divers supports tout en gardant une lisibilité et une navigation impeccable, sans dégradation des fonctionnalitées du site web.
+                            With that information in mind, it was crucial for our solution to be responsive and adapted to mobile supports, while being as easy to use and navigate as on a computer and keeping all the website's features.
                         </p>
                     </>,
                 fr : 
--- src/view/projects/toetcie/ToetcieProjectOrder.tsx
+++ src/view/projects/toetcie/ToetcieProjectOrder.tsx
@@ -23,16 +23,16 @@ const ToetcieProjectOrder : React.FC = props => {
                 en : 
                     <>
                         <p>
-                            Une des fonctionnalités principales et la plus attendue par le restaurateur, la commande en ligne permet aux clients de passer leur commande directement sur le site web.
+                            One of the main feature, which was also key for the client, is the online ordering. Thus customers are able to order directly through the website without having to call on the phone, which was the current solution at the time.
                         </p>
                         <p>
-                            Ce module à été réalisé de sorte à minimiser les ambiguïtés de prix ou du contenue même de la commanque, que ce soit du côté client ou restaurateur. 
-                            Les montants sont affichés tout au long de la commande avec des notes explicites sur le prix de chaque élément. 
-                            Le restaurateur quant à lui reçoit un e-mail standardisé avec le détail de la commande, qui lui permet ensuite de très rapidement lire et assimiler la demande du client.
+                            This module was made in order to reduce mistakes on the content and to clear any doubt about the prices. 
+                            Indeed, they are clearly displayed throughout the whole process.
+                            Once the order is validated by the customer, a standardised email is automatically sent to the restaurant with the order's details, such as the name of the client and the pick up time.
                         </p>
                         <p>
-                            Pour la réalisation, nous avons retenu une solution basée sur l'envoi d'e-mail et sans serveur.
-                            Cette démarche permettant une réduction significative des coûts d'hébergement pour le restaurateur.
+                            For this feature, our solution is based on a serverless architecture.
+                            This choice allows for a massive saving in terms of hosting for the restaurant owner.
                         </p>
                     </>,
                 fr : 
@@ -41,13 +41,13 @@ const ToetcieProjectOrder : React.FC = props => {
                             Une des fonctionnalités principales et la plus attendue par le restaurateur, la commande en ligne permet aux clients de passer leur commande directement sur le site web.
                         </p>
                         <p>
-                            Ce module à été réalisé de sorte à minimiser les ambiguïtés de prix ou du contenue même de la commanque, que ce soit du côté client ou restaurateur. 
+                            Ce module a été réalisé de sorte à minimiser les ambiguïtés de prix ou du contenu même de la commande, que ce soit du côté client ou restaurateur. 
                             Les montants sont affichés tout au long de la commande avec des notes explicites sur le prix de chaque élément. 
                             Le restaurateur quant à lui reçoit un e-mail standardisé avec le détail de la commande, qui lui permet ensuite de très rapidement lire et assimiler la demande du client.
                         </p>
                         <p>
                             Pour la réalisation, nous avons retenu une solution basée sur l'envoi d'e-mail et sans serveur.
-                            Cette démarche permettant une réduction significative des coûts d'hébergement pour le restaurateur.
+                            Cette démarche permet une réduction significative des coûts d'hébergement pour le restaurateur.
                         </p>
                     </>
             }}
